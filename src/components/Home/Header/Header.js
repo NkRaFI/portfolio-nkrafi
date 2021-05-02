9a82673752afdc87d57bbadb44f4e1f48ac69cab
NkRaFI <nkrafi11@gmail.com>
2021-05-02 20:57:43 +0600

    finished creating porfolio-nkrafi

--- src/components/Home/Header/Header.js
+++ src/components/Home/Header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import myPicture from '../../../images/profile-picture-orange.png';
 import Navbar from '../../Navbar/Navbar';
 import './Header.css';
@@ -14,7 +15,7 @@ const Header = () => {
                             <h1 className="primaryColor headerName">Nk Rafi</h1>
                             <p className="text-secondary">Web Developer | Programmer | UI/UX Designer </p>
                             <p className="my-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse blanditiis nulla ut sapiente voluptate unde molestiae provident autem. Nobis, necessitatibus!</p>
-                            <button className="btn primaryBg">Contact Me</button>
+                            <Link to="/contact"><button className="btn primaryBg">Contact Me</button></Link>
                         </div>
                     </div>
                     <div className="col-12 col-md-6 d-flex justify-content-center align-items-center my-4">
